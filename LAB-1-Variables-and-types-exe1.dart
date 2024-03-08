755d9d184cbfb4ad4d14ba2f44266a09aceaecc6
Maedoot Asrat <asratmaedot@gmail.com>
2024-03-08 17:34:23 +0300

    Variables and types exercise 1

---
+++ LAB-1-Variables-and-types-exe1.dart
@@ -0,0 +1,9 @@
+
+void main() {
+  String name = 'Maedot Asrat';
+  int age = 21;
+  String favoriteColor = 'blue';
+
+  print('I am $name.\nI am $age years old.\nMy favorite color is $favoriteColor.');
+}
+
